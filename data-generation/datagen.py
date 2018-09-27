2f4ce6e4dd864045fc6d53f43ca5e532a71d6a4c
Jaroslav Hlavac <hlavac.jaroslav@gmail.com>
2018-09-27 14:28:22 +0200

    generation of data used for similarity calculating

---
+++ data-generation/datagen.py
@@ -0,0 +1,45 @@
+import json
+from random import randint
+
+#-------------------------------------------------------------------------
+# filling table = for size = 4
+#  XX | a0 | a1 | a2 | a3
+# ------------------------
+#  a0 | XX |    |    |
+# ------------------------
+#  a1 |fill| XX |    |
+# ------------------------
+#  a2 |fill|fill| XX |
+# ------------------------
+#  a3 |fill|fill|fill| XX
+# ------------------------
+def generate_similarity_tables(size):
+    
+    ret_dict = {}
+    id_num = 0
+    while id_num != size: #generate identificators
+        ret_dict.update({"a" + str(id_num) : []})
+        id_num += 1
+
+    values = [] #communication of values between hosts
+    i = 0
+    for row in ret_dict:
+        for column in ret_dict:
+            if(row == column):
+                continue
+            app = randint(0,1000)
+            if (app > 800):
+                ret_dict[row].append({column:app})
+                ret_dict[column].append({row:app})
+    return ret_dict
+
+#-------------------------------------------------------------------------
+
+print("Starting to generate data")
+
+data = generate_similarity_tables(1000)
+
+
+output_file = open("data.json", 'w')
+
+output_file.write(json.dumps(data))
