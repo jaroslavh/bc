7c200f20ce89e1279180ae1c53c37d0d593d234d
Jaroslav Hlavac <hlavac.jaroslav@gmail.com>
2018-10-04 15:03:46 +0200

    archived basic datagen, will be simplified

--- data-generation/datagen.py
+++ data-generation/datagen.py
@@ -2,7 +2,7 @@ import json
 from random import randint
 
 #-------------------------------------------------------------------------
-# filling table = for size = 4
+# filling table example for size 4
 #  XX | a0 | a1 | a2 | a3
 # ------------------------
 #  a0 | XX |    |    |
@@ -13,22 +13,23 @@ from random import randint
 # ------------------------
 #  a3 |fill|fill|fill| XX
 # ------------------------
+# from this table if the bracket fill is higher than 800, then I create a subtable with the
+# row and column
+# returns { id : {id_of_peer : num_of_hits }}
 def generate_similarity_tables(size):
     
     ret_dict = {}
     id_num = 0
-    while id_num != size: #generate identificators
+    while id_num != size: # generate identificators field
         ret_dict.update({"a" + str(id_num) : []})
         id_num += 1
 
-    values = [] #communication of values between hosts
-    i = 0
     for row in ret_dict:
         for column in ret_dict:
             if(row == column):
                 continue
             app = randint(0,1000)
-            if (app > 800):
+            if (app > 800): # set threshold for data to be taken into account
                 ret_dict[row].append({column:app})
                 ret_dict[column].append({row:app})
     return ret_dict
